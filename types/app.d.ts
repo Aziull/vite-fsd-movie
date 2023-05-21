db4a68ca8e3cab6fc9e802986f497dc0660decc7
Aziul <y.nazar9@gmail.com>
2023-05-22 01:38:52 +0300

    app config

--- types/app.d.ts
+++ types/app.d.ts
@@ -1,8 +1,13 @@
 declare global {
 
-    export type Email = string
+    export type Brand<K, T> = K & { [_brand]: T };
 
+    export type Email = string
+    export type Username = string
     export type Id = number
+
+    export type Title = string
+    export type Url = str
 
     declare type RootState = import('../src/app/redux/store').RootState
     declare type AppDispatch = import('../src/app/redux/store').AppDispatch
